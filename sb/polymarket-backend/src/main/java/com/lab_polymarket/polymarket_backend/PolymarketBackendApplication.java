0a93cb43a34ec29a1bb3c44113aa27cb9de5f006
EricBM19 <ericbm19@gmail.com>
2026-05-05 00:06:06 +0200

    Code: First Spring Boot commit with database connection.

---
+++ sb/polymarket-backend/src/main/java/com/lab_polymarket/polymarket_backend/PolymarketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lab_polymarket.polymarket_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolymarketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolymarketBackendApplication.class, args);
+	}
+
+}
